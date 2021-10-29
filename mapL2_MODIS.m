c4b2a2a948b66c4b4672b962e2add23decf744cf
ocean-slang <slang@uri.edu>
2021-10-29 18:15:16 -0400

    mapping script and sample level-2

---
+++ mapL2_MODIS.m
@@ -0,0 +1,66 @@
+%% Read data
+cd '/Users/sarahlang/Desktop/mapL2/';
+
+% Add functions to path
+addpath('/Users/sarahlang/Desktop/mapL2/functions')
+addpath('/Users/sarahlang/Desktop/mapL2/functions/m_map')
+load coastlines
+
+% Loop Through Directory and Write Coordinates to a Structure
+sdir = dir('/Users/sarahlang/Desktop/mapL2/data/*OC');
+spath = '/Users/sarahlang/Desktop/mapL2/data/';
+
+% Note: If processing data from a different satellite, check
+% parameters using ncdisp('<filename>'). I think they should all be the
+% same, but if you get an error about a missing variable, this may be why.
+%% Loop through directory, map parameter of interest
+for file = 1:length(sdir)
+    
+    cd(spath)
+    
+    % Read Files
+    filename = sdir(file).name;
+    
+    chlora=ncread(filename,'/geophysical_data/chlor_a');
+%   pic=ncread(filename,'pic');
+%   poc=ncread(filename,'poc');
+    lat=ncread(filename,'/navigation_data/latitude');
+    lon=ncread(filename,'/navigation_data/longitude');
+
+    % Subset data
+    latmatches = (lat >= 38 & lat <= 40); %define bounds of latitude
+    longmatches = (lon >= -125 & lon <= -123); %define bounds of longitude
+    subset = (longmatches==1 & latmatches==1); %create logical array for subsetting
+    chl=chlora.*subset;
+    chl(chl==0)= NaN;
+    longitude=lon.*subset;
+    lon(lon==0)= NaN;
+    latitude=lat.*subset;
+    lat(lat==0)= NaN;
+    
+ 
+    LATLIMS=[38 40];
+    LONLIMS=[-125 -123];
+   % bndry_lon=[-128 -128 -122 -122 -128]; %CHANGE BOUNDING BOX VIA MELISSAS EMAIL
+   % bndry_lat=[34 40 40 34 34];
+    
+    %get date for plot titles
+    julianday = filename(6:8);
+    t=datestr(datenum(2021,0, str2num(julianday)));
+    
+    figure()
+    m_proj('Mercator','lon',LONLIMS,'lat',LATLIMS);
+    m_pcolor(lon, lat, chl);shading flat;
+    m_grid('linewi',2,'tickdir','out');
+    h=colorbar;
+    m_coast('linewidth',0.5,'color','k');
+    %m_coast('patch',[0.5 0.5 0.5]);
+    set(get(h,'ylabel'),'String','Chlorophyll-a (mg/m^{3})');
+    caxis([0 1]) %change to reflect what chlorophyll concs you are working with
+    ylabel('Latitude ( Â°)')
+    xlabel('Longitude ( Â°)')
+    title([t])
+    set(gca,'FontSize',14);  
+    %m_line(bndry_lon,bndry_lat,'linewi',1,'color','k');
+    print(gcf,[t,'_chl.pdf'],'-dpdf','-r300'); 
+end
